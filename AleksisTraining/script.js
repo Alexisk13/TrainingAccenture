f97870a81d2db397e161c1107ebd2ddad07c5367
Alexisk13 <152257574+Alexisk13@users.noreply.github.com>
2024-07-08 10:30:28 +0000

    added js pop up

---
+++ AleksisTraining/script.js
@@ -0,0 +1,8 @@
+var name1 = prompt("Input your name: ");
+var surname = prompt("Input your last name: ");
+
+if (name1 === "Aleksis" && surname === "Kahanovskis"){
+    alert("Hello, owner of the cv!")
+} else{
+    alert("Hello, viewer of the cv!")
+}
